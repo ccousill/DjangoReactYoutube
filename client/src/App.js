b01bf3327517891a422d5b8da1aeba5473a96866
ccousill <chriscous00@gmail.com>
2023-12-18 16:36:09 -0500

    simple nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import './css/navbar.css'
 import Home from './components/Home'
-
+import Navbar from './components/UI/Navbar';
 function App() {
   return (
+    <div className="wrapper">
+    <Navbar/>
     <Home/>
+    </div>
   );
 }
 
---
+++ client/src/components/UI/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+function Navbar() {
+  return (
+    <header className="header">
+      <p>YouTube</p>
+
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Videos</li>
+        </ul>
+      </nav>
+
+    </header>
+  );
+}
+
+export default Navbar;
