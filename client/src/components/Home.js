61c70213d679466c2dd95c045e1af8915817d1bf
ccousill <chriscous00@gmail.com>
2024-01-01 13:54:29 -0500

    button additions to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="home-section">
       <div className="home-info">
         <h1>Find filtered videos and statistics for the day</h1>
+        <h3>Start here and travel through to site</h3>
+        <div className="home-buttons">
+          <Link to="/" className="button-1">
+            Travel to destination
+          </Link>
+          <Link to="/" className="button-2">
+            Travel to destination
+          </Link>
+        </div>
       </div>
+
       <div className="home-videos">Preview Videos</div>
     </div>
   );
