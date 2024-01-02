1b52e31e2bf9603705101c800c6236a9099cac16
ccousill <chriscous00@gmail.com>
2024-01-02 16:14:18 -0500

    minor templating

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,9 @@ function Home() {
     <div className="home-section">
       <div className="home-info">
         <h1>Find filtered videos and statistics for the day</h1>
-        <h3>Start here and travel through to site</h3>
+        <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+</h3>
         <div className="home-buttons">
           <Link to="/" className="button-1">
             Travel to destination
